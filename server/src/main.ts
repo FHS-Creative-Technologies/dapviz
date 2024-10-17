bd5db521df711baae40bad5c1bfb128c73521186
MaKhandare <khandare.matthaeus@gmail.com>
2024-10-17 18:35:52 +0200

    setup websocket on vite proxy

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,24 +1,11 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from './vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const ws = new WebSocket(`ws://${location.host}/api/events`);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+ws.addEventListener("open", (_e) => {
+  console.log("connected to ws");
+})
+
+ws.addEventListener("message", (e) => {
+  console.log("msg from ws: ", e.data);
+});
