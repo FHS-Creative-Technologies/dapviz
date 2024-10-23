b44b03f2ea5ce0ef19e8d42d7f4a3b64ee45862d
Leo Katzengruber <leofel.katz@gmail.com>
2024-10-23 13:45:19 +0200

    add example program in playground with output

--- playground/csharp/Program.cs
+++ playground/csharp/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+var mat = new Matrix() {
+    Row0 = new Vector{
+        X = 0,
+        Y = 0,
+        Z = 0,
+    },
+    Row1 = new Vector{
+        X = 0,
+        Y = 0,
+        Z = 0,
+    },
+    Row2 = new Vector{
+        X = 0,
+        Y = 0,
+        Z = 0,
+    },
+};
+
+for (int i = 0; i < 10; ++i) {
+    Console.Write(i);
+}
+
+Console.WriteLine("Hello, World");
+
+class Vector {
+    public int X{get; init; }
+    public int Y{get; init; }
+    public int Z {get; init; }
+}
+class Matrix {
+    public required Vector Row0 {get; init;}
+    public required Vector Row1{get; init;}
+    public required Vector Row2{get; init;}
+}
