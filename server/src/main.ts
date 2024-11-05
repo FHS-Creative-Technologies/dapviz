5cb50c8878ab3cf00a3c73222d5e1bf5d368c1f1
MaKhandare <khandare.matthaeus@gmail.com>
2024-11-05 11:27:30 +0100

    init stack variables

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,56 @@
 const ws = new WebSocket(`ws://${location.host}/api/events`);
 
+interface Variable {
+  name: string;
+  value: string;
+  type: string;
+  variablesReference: number; // do we need this?
+  memoryReference?: string; // do we need this?
+}
+
+// TODO: handle heap data somehow
+const variablesData = {
+  stack: [] as Variable[],
+  // heap: new Map<string, Variable>()
+}
+
+function storeVariables(variables: Variable[]) {
+  variables.forEach(variable => {
+    if (isStackVariable(variable)) {
+      variablesData.stack.push(variable);
+    } else {
+      // handle heap data
+      // variablesData.heap.set(variable.name, variable);
+    }
+  })
+}
+
+// TODO: maybe there is a way to check with memoryRef oder varRef
+function isStackVariable(variable: Variable): boolean {
+  return variable.type === "int" || variable.type === "float"
+    || variable.type === "char" || variable.type === "double";
+}
+
 ws.addEventListener("open", (_e) => {
   console.log("connected to ws");
 })
 
 ws.addEventListener("message", (e) => {
   try {
-    console.log(JSON.parse(e.data));
+    const data = JSON.parse(e.data);
+
+    if (data.type === "response" && data.command === "variables" && data.success) {
+
+      const variables = data.body.variables as Variable[];
+      console.log(`received: ${variables}`);
+
+      storeVariables(variables);
+
+      console.log("Stack Variables:", variablesData.stack);
+      // console.log("Heap Variables:", Array.from(variablesData.heap.entries()));
+
+    }
+
   } catch (err) {
     console.error("Invalid JSON:", e.data, err);
   }
