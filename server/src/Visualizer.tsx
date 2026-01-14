6d94fe9eceb88bffda6d015512a17055ae5c9e84
Leo Katzengruber <leofel.katz@gmail.com>
2026-01-14 11:20:43 +0100

    disable heap connections

--- server/src/Visualizer.tsx
+++ server/src/Visualizer.tsx
@@ -8,9 +8,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "./components/default/accordion";
-import { HeapConnectionsProvider, useHeapConnections } from "./HeapConnectionsProvider";
-import * as THREE from "three";
-import { useEffect, useMemo, useRef } from "react";
+import { useMemo } from "react";
 import { useTheme } from "./ThemeProvider";
 
 const BackgroundGrid = () => {
@@ -38,23 +36,6 @@ const StackFrameVariable = ({ variable }: { variable: Variable }) => {
   const theme = useTheme();
   return (
     <Container flexDirection="row" justifyContent="space-between" width="auto">
-      <Text fontSize={16} color={theme.text.primary}>
-        {variable.name}
-      </Text>
-      <Text fontSize={16} color={theme.text.primary}>
-        {variable.value}
-      </Text>
-    </Container>
-  );
-};
-
-const PrimitiveVariable = ({ variable }: { variable: Variable }) => {
-  const theme = useTheme();
-  return (
-    <Container flexDirection="row" justifyContent="space-around" width="auto" renderOrder={1}>
-      <Text fontSize={16} color={theme.text.type}>
-        {variable.type}
-      </Text>
       <Text fontSize={16} color={theme.text.primary}>
         {variable.name}
       </Text>
@@ -104,146 +85,7 @@ const Stack = ({ thread }: { thread: ThreadInfo }) => {
   );
 };
 
-const HeapNode = ({
-  variable,
-  childrenMap,
-  initialPosition,
-}: {
-  variable: Variable;
-  childrenMap: Map<number | null, Variable[]>;
-  initialPosition: [number, number, number];
-}) => {
-  const theme = useTheme();
-  const groupRef = useRef<THREE.Group>(null);
-
-  const { registerNode, unregisterNode } = useHeapConnections();
-
-  useEffect(() => {
-    if (variable.reference > 0 && groupRef.current) {
-      registerNode(variable.reference, groupRef);
-    }
-
-    return () => {
-      if (variable.reference > 0) {
-        unregisterNode(variable.reference);
-      }
-    };
-  }, [variable.reference, registerNode, unregisterNode]);
-
-  const { primitiveChildren, referenceChildren } = useMemo(() => {
-    const children = childrenMap.get(variable.reference) ?? [];
-
-    const primitiveChildren: Variable[] = [];
-    const referenceChildren: Variable[] = [];
-
-    for (const child of children) {
-      if (child.reference === 0) {
-        primitiveChildren.push(child);
-      } else {
-        referenceChildren.push(child);
-      }
-    }
-
-    return { primitiveChildren, referenceChildren };
-  }, [childrenMap, variable.reference]);
-
-  const xOffset = 250;
-  const yOffset = 100;
-
-  return (
-    <>
-      <DragControls>
-        <group ref={groupRef} position={initialPosition}>
-          <Root justifyContent="flex-start" flexDirection="column" pixelSize={0.5}>
-            <DefaultProperties fontWeight="medium">
-              <Container
-                flexDirection="column"
-                backgroundColor={theme.node.background}
-                hover={{ backgroundColor: theme.node.backgroundHover }}
-                padding={15}
-                borderRadius={12}
-                borderWidth={1}
-                borderColor={theme.node.border}
-              >
-                <Container
-                  flexDirection="row"
-                  justifyContent="space-evenly"
-                  alignItems="center"
-                  paddingBottom={10}
-                >
-                  <Text
-                    fontSize={22}
-                    color={theme.text.primary}
-                    fontWeight="bold"
-                    paddingRight={24}
-                    renderOrder={1}
-                  >
-                    {variable.name}
-                  </Text>
-                  <Text
-                    fontSize={16}
-                    color={theme.text.secondary}
-                    fontWeight="thin"
-                    renderOrder={1}
-                  >
-                    {variable.type}
-                  </Text>
-                </Container>
-
-                <Container height={1} backgroundColor={theme.node.divider} marginY={4} />
-
-                <Container flexDirection="column" marginTop={14} gap={8}>
-                  {primitiveChildren.map((child) => (
-                    <PrimitiveVariable key={child.name} variable={child} />
-                  ))}
-                </Container>
-              </Container>
-            </DefaultProperties>
-          </Root>
-        </group>
-      </DragControls>
-
-      {referenceChildren.map((child, index) => {
-        const childPosition: [number, number, number] = [
-          initialPosition[0] + xOffset,
-          initialPosition[1] - index * yOffset,
-          initialPosition[2],
-        ];
-
-        return (
-          <HeapNode
-            key={child.name}
-            variable={child}
-            childrenMap={childrenMap}
-            initialPosition={childPosition}
-          />
-        );
-      })}
-    </>
-  );
-};
-
 const Visualizer = ({ thread }: { thread: ThreadInfo }) => {
-  const allVariables = useMemo(
-    () => thread.stack_frames.flatMap((frame) => frame.scopes.flatMap((scope) => scope.variables)),
-    [thread.stack_frames],
-  );
-
-  const { rootHeapVariables, childrenMap } = useMemo(() => {
-    const childrenMap = new Map<number | null, Variable[]>();
-
-    for (const variable of allVariables) {
-      const children = childrenMap.get(variable.parent) ?? [];
-      children.push(variable);
-      childrenMap.set(variable.parent, children);
-    }
-
-    const rootVariables = childrenMap.get(null) ?? [];
-    const rootHeapVariables = rootVariables.filter((v) => v.reference > 0);
-
-    return { rootHeapVariables, childrenMap };
-  }, [allVariables]);
-
   return (
     <>
       <DebugJsonInfo thread={thread} />
@@ -260,17 +102,6 @@ const Visualizer = ({ thread }: { thread: ThreadInfo }) => {
           </DefaultProperties>
         </Root>
       </DragControls>
-
-      <HeapConnectionsProvider allVariables={allVariables}>
-        {rootHeapVariables.map((variable, index) => (
-          <HeapNode
-            key={variable.name}
-            variable={variable}
-            childrenMap={childrenMap}
-            initialPosition={[300, index * -200, 0]}
-          />
-        ))}
-      </HeapConnectionsProvider>
 
       <BackgroundGrid />
       <MapControls maxZoom={2} minZoom={0.1} makeDefault />
