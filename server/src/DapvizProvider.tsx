2cc7fe8e2ef854d858f5855ddd6bb6b85493b35c
Leo Katzengruber <leofel.katz@gmail.com>
2026-02-16 22:02:44 +0100

    remove react-three-fiber dependency

--- server/src/DapvizProvider.tsx
+++ server/src/DapvizProvider.tsx
@@ -3,38 +3,38 @@ import { createContext, ReactElement, ReactNode, useContext, useEffect, useState
 
 // align these values with `impl TryFrom<Message> for UserRequest` block
 export enum DapvizRequest {
-  Step = 1,
-  StepIn = 2,
-  StepOut = 3,
+    Step = 1,
+    StepIn = 2,
+    StepOut = 3,
 }
 
 export type Variable = {
-  parent: number | null;
-  reference: number;
-  name: string;
-  value: string;
-  type: string;
+    parent: number | null;
+    reference: number;
+    name: string;
+    value: string;
+    type: string;
 };
 
 export type Scope = {
-  variables: [Variable];
+    variables: [Variable];
 };
 
 export type StackFrame = {
-  file: string;
-  line: number;
-  function: string;
-  scopes: [Scope];
+    file: string;
+    line: number;
+    function: string;
+    scopes: [Scope];
 };
 
 export type ThreadInfo = {
-  id: number;
-  name: string;
-  stack_frames: [StackFrame];
+    id: number;
+    name: string;
+    stack_frames: [StackFrame];
 };
 
 export type ProgramState = {
-  threads: [ThreadInfo];
+    threads: [ThreadInfo];
 };
 
 type DapvizRequestFunction = (request: DapvizRequest, threadId: number) => void;
@@ -42,72 +42,72 @@ type DapvizRequestFunction = (request: DapvizRequest, threadId: number) => void;
 const DapvizContext = createContext<[ProgramState, DapvizRequestFunction] | null>(null);
 
 export const useDapviz = () => {
-  const data = useContext(DapvizContext);
-  if (!data) {
-    throw Error("useDapviz called outside of DapvizProvider");
-  }
+    const data = useContext(DapvizContext);
+    if (!data) {
+        throw Error("useDapviz called outside of DapvizProvider");
+    }
 
-  return data;
+    return data;
 };
 
 const DapvizProvider = ({
-  children,
-  noConnection,
+    children,
+    noConnection,
 }: {
-  children: ReactNode;
-  noConnection: ReactElement;
+    children: ReactNode;
+    noConnection: ReactElement;
 }) => {
-  const [requestFunction, setRequestFunction] = useState<DapvizRequestFunction | null>(null);
-  const [programState, setProgramState] = useState<ProgramState | null>(null);
+    const [requestFunction, setRequestFunction] = useState<DapvizRequestFunction | null>(null);
+    const [programState, setProgramState] = useState<ProgramState | null>(null);
 
-  useEffect(() => {
-    const ws = new WebSocket(`ws://${location.host}/api/events`);
+    useEffect(() => {
+        const ws = new WebSocket(`ws://${location.host}/api/events`);
 
-    // NOTE: need to use the react state set function overload to set react state to a function
-    // https://stackoverflow.com/a/55621325/7482275
-    ws.addEventListener("open", () =>
-      setRequestFunction(() => (request: DapvizRequest, threadId: number) => {
-        const data = new ArrayBuffer(9);
-        const view = new DataView(data);
+        // NOTE: need to use the react state set function overload to set react state to a function
+        // https://stackoverflow.com/a/55621325/7482275
+        ws.addEventListener("open", () =>
+            setRequestFunction(() => (request: DapvizRequest, threadId: number) => {
+                const data = new ArrayBuffer(9);
+                const view = new DataView(data);
 
-        view.setBigInt64(0, BigInt(threadId), true);
-        view.setInt8(8, request);
+                view.setBigInt64(0, BigInt(threadId), true);
+                view.setInt8(8, request);
 
-        ws.send(data);
-      }),
+                ws.send(data);
+            }),
+        );
+
+        ws.addEventListener("message", (e) => {
+            let json;
+            try {
+                json = JSON.parse(e.data);
+            } catch (err) {
+                console.error("Invalid JSON:", e.data, err);
+                return;
+            }
+
+            if (json?.threads?.length > 0) {
+                setProgramState(json);
+            } else {
+                setProgramState(null);
+            }
+        });
+
+        ws.addEventListener("close", () => {
+            setRequestFunction(null);
+            setProgramState(null);
+        });
+
+        return () => ws.close();
+    }, []);
+
+    return requestFunction !== null && programState !== null ? (
+        <DapvizContext.Provider value={[programState, requestFunction]}>
+            {children}
+        </DapvizContext.Provider>
+    ) : (
+        noConnection
     );
-
-    ws.addEventListener("message", (e) => {
-      let json;
-      try {
-        json = JSON.parse(e.data);
-      } catch (err) {
-        console.error("Invalid JSON:", e.data, err);
-        return;
-      }
-
-      if (json?.threads?.length > 0) {
-        setProgramState(json);
-      } else {
-        setProgramState(null);
-      }
-    });
-
-    ws.addEventListener("close", () => {
-      setRequestFunction(null);
-      setProgramState(null);
-    });
-
-    return () => ws.close();
-  }, []);
-
-  return requestFunction !== null && programState !== null ? (
-    <DapvizContext.Provider value={[programState, requestFunction]}>
-      {children}
-    </DapvizContext.Provider>
-  ) : (
-    noConnection
-  );
 };
 
 export default DapvizProvider;
--- server/src/Visualizer.tsx
+++ server/src/Visualizer.tsx
@@ -1,112 +1,12 @@
-import { DragControls, Grid, MapControls } from "@react-three/drei";
-import { StackFrame, ThreadInfo, Variable } from "./DapvizProvider";
-import DebugJsonInfo from "./DebugJsonInfo";
-import { Container, DefaultProperties, Root, Text } from "@react-three/uikit";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "./components/default/accordion";
-import { useMemo } from "react";
-import { useTheme } from "./ThemeProvider";
-
-const BackgroundGrid = () => {
-  const theme = useTheme();
-  const size = 100;
-  const thickness = 1.5;
-  return (
-    <mesh rotation={[Math.PI / 2, 0, 0]} position={[0, 0, -10]}>
-      <Grid
-        cellColor={theme.grid.cell}
-        cellThickness={thickness / 1.5}
-        sectionColor={theme.grid.section}
-        sectionThickness={thickness}
-        cellSize={size / 2}
-        sectionSize={size}
-        fadeDistance={50000}
-        fadeStrength={1.0}
-        infiniteGrid
-      />
-    </mesh>
-  );
-};
-
-const StackFrameVariable = ({ variable }: { variable: Variable }) => {
-  const theme = useTheme();
-  return (
-    <Container flexDirection="row" justifyContent="space-between" width="auto">
-      <Text fontSize={16} color={theme.text.primary}>
-        {variable.name}
-      </Text>
-      <Text fontSize={16} color={theme.text.primary}>
-        {variable.value}
-      </Text>
-    </Container>
-  );
-};
-
-const StackFrameViz = ({ stackFrame }: { stackFrame: StackFrame }) => {
-  const rootVariables = useMemo(() => {
-    const allFrameVariables = stackFrame.scopes.flatMap((scope) => scope.variables);
-    return allFrameVariables.filter((variable) => variable.parent === null);
-  }, [stackFrame.scopes]);
-
-  return (
-    <Container flexDirection="column-reverse" flexGrow={1}>
-      <Accordion width="auto">
-        {rootVariables.map((variable) => (
-          <StackFrameVariable key={variable.name} variable={variable} />
-        ))}
-      </Accordion>
-    </Container>
-  );
-};
-
-const Stack = ({ thread }: { thread: ThreadInfo }) => {
-  const theme = useTheme();
-  return (
-    <Container flexDirection="column" width="auto">
-      <Accordion width="auto">
-        {thread.stack_frames.map((stackFrame, i) => (
-          <AccordionItem key={i} width="auto" value={stackFrame.function}>
-            <AccordionTrigger>
-              <Text fontSize={24} color={theme.text.primary}>
-                {stackFrame.function}
-              </Text>
-            </AccordionTrigger>
-            <AccordionContent width="auto">
-              <StackFrameViz stackFrame={stackFrame} />
-            </AccordionContent>
-          </AccordionItem>
-        ))}
-      </Accordion>
-    </Container>
-  );
-};
+import { ThreadInfo } from "./DapvizProvider";
 
 const Visualizer = ({ thread }: { thread: ThreadInfo }) => {
-  return (
-    <>
-      <DebugJsonInfo thread={thread} />
-      <DragControls>
-        <Root
-          sizeX={175}
-          sizeY={500}
-          justifyContent="flex-end"
-          flexDirection="column"
-          pixelSize={0.5}
-        >
-          <DefaultProperties fontWeight="medium">
-            <Stack thread={thread} />
-          </DefaultProperties>
-        </Root>
-      </DragControls>
-
-      <BackgroundGrid />
-      <MapControls maxZoom={2} minZoom={0.1} makeDefault />
-    </>
-  );
+    return (
+        <details>
+            <summary>Debug</summary>
+            <pre>{JSON.stringify(thread, null, 2)}</pre>
+        </details>
+    );
 };
 
 export default Visualizer;
